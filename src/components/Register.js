23dcd87ee039dac4ad0d482333ef59222776f03c
Maksim Zakharchuk <mad_max0100@mail.ru>
2023-03-21 18:05:19 +0300

    feat: сверстать компонент Register

---
+++ src/components/Register.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { auth } from "../utils/auth";
+
+const Register = () => {
+  const [formValue, setFormValue] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setFormValue({
+      ...formValue,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-auth">
+      <h2 className="form-auth__heading">Регистрация</h2>
+      <input
+        className="form-auth__input"
+        id="user-email-input"
+        name="email"
+        value={formValue.email || ''}
+        onChange={handleChange}
+        type="email"
+        placeholder="Email"
+        minLength="2"
+        maxLength="40"
+        required
+      />
+      <input
+        className="form-auth__input"
+        id="user-password-input"
+        name="password"
+        value={formValue.password || ''}
+        onChange={handleChange}
+        type="password"
+        placeholder="Пароль"
+        minLength="6"
+        maxLength="200"
+        required
+      />
+      <button
+        type="submit"
+        className="form-auth__save-button"
+      >
+        Зарегистрироваться
+      </button>
+      <div className="form-auth__question">
+        <span>Уже зарегистрированы? </span>
+        <Link to="/sign-in" className="form-auth__link">Войти</Link>
+      </div>
+    </form>
+  );
+};
+
+export default Register;
